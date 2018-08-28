474d60d27686b1d19c33a52d7d907b454090d197
HuaiZ <apokar@163.com>
2018-08-28 16:51:51 +0800

    添加点击量记录

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -2,19 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.six import python_2_unicode_compatible
 from django.urls import reverse
+
+
 # Create your models here.
 
 @python_2_unicode_compatible
 class Category(models.Model):
     def __str__(self):
         return self.name
+
     name = models.CharField(max_length=100)
+
 
 @python_2_unicode_compatible
 class Tag(models.Model):
     def __str__(self):
         return self.name
+
     name = models.CharField(max_length=100)
+
 
 @python_2_unicode_compatible
 class Post(models.Model):
@@ -22,8 +28,8 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('blog:detail',kwargs={'pk':self.pk})
-        
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+
     title = models.CharField(max_length=90)
 
     body = models.TextField()
@@ -31,7 +37,7 @@ class Post(models.Model):
     created_time = models.DateTimeField()
     modified_time = models.DateTimeField()
 
-    excerpt = models.CharField(max_length=200,blank = True)
+    excerpt = models.CharField(max_length=200, blank=True)
 
     category = models.ForeignKey(Category)
 
@@ -39,9 +45,12 @@ class Post(models.Model):
 
     author = models.ForeignKey(User)
 
+    # 新增 views 字段记录阅读量
+    views = models.PositiveIntegerField(default=0)
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
     class Meta:
         ordering = ['-created_time', 'title']
-        
-
-
-    
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 from django.http import HttpResponse
 
 import markdown
 from comments.forms import CommentForm
 
+
+
 # Create your views here.
 
-#def index(request):
+# def index(request):
 #    return render(request,'blog/index.html',context={
 #            "title":'my blog',
 #            "welcome":"welcome to apokar's blog~!"
@@ -16,17 +18,21 @@ from comments.forms import CommentForm
 def index(request):
     # post_list = Post.objects.all().order_by('-created_time')
     post_list = Post.objects.all()
-    return render(request,'blog/index.html',context = {'post_list':post_list})
+    return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     # 记得在顶部引入 markdown 模块
+
+    # 阅读量 +1
+    post.increase_views()
+
     post.body = markdown.markdown(post.body,
                                   extensions=[
-                                     'markdown.extensions.extra',
-                                     'markdown.extensions.codehilite',
-                                     'markdown.extensions.toc',
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
                                   ])
 
     form = CommentForm()
